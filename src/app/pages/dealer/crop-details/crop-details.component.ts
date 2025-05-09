6e3f4d1e82c5cb8cf38b0f9436169a05f91480e5
yash-mewada <yashmewada27@gmail.com>
2025-05-09 15:34:25 +0530

    added crop-details page

--- src/app/pages/dealer/crop-details/crop-details.component.ts
+++ src/app/pages/dealer/crop-details/crop-details.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-crop-details',
@@ -10,16 +11,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CropDetailsComponent implements OnInit {
   crop: any;
+  isLoading: boolean = true;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
     const cropId = this.route.snapshot.paramMap.get('id');
-    // Ideally make an API call to fetch by ID. For now just simulate:
-    this.crop = history.state.cropData || {}; // You can enhance this
+    if (cropId) {
+      this.fetchCropDetails(cropId);
+    }
+  }
+
+  fetchCropDetails(cropId: string): void {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    this.http.get<any>(`http://localhost:5180/api/CropListing/${cropId}/details`, { headers })
+      .subscribe({
+        next: (response) => {
+          this.crop = response;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error("Failed to fetch crop details", err);
+          this.isLoading = false;
+        }
+      });
   }
 
   buyNow(): void {
-    alert("Buy Now clicked!");
+    alert(`You have initiated the purchase of ${this.crop.quantity} Kg of ${this.crop.cropName}.`);
   }
 }
